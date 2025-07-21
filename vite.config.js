b5f6025a18366f21cfa10abc1bc939622342e9e9
Swapnil5314 <rajy96751@gmail.com>
2025-07-21 22:26:06 +0530

    Fix vite base and index path for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈🏽 this fixes 404 on Vercel
-});
+  base: './', // 👈 Vercel ke liye important hai!
+})
